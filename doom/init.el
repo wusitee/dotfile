0d3a4f224e83cf381cdb3c63899c972139daf9f8
wste <20260210@tsinglan.cn>
2024-10-25 23:00:32 +0800

    feat(doom): enable lsp for dart

--- doom/init.el
+++ doom/init.el
@@ -123,7 +123,7 @@
        ;;crystal           ; ruby at the speed of c
        ;;csharp            ; unity, .NET, and mono shenanigans
        ;;data              ; config/data formats
-       ;;(dart +frutter)   ; paint ui and not much else
+       (dart +flutter +lsp)   ; paint ui and not much else
        ;;dhall
        ;;elixir            ; erlang done right
        ;;elm               ; care for a cup of TEA?
