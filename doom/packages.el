22676130b4c38e9233564d7f46c910295452e260
wste <20260210@tsinglan.cn>
2025-01-04 14:27:42 +0800

    refactor(doom): use org dev branch to preview tex

--- doom/packages.el
+++ doom/packages.el
@@ -50,13 +50,35 @@
 ;; (unpin! t)
 
 (package! tldr)
+(package! org :recipe
+  (:host nil :repo "https://git.tecosaur.net/mirrors/org-mode.git" :remote "mirror" :fork
+   (:host nil :repo "https://git.tecosaur.net/tec/org-mode.git" :branch "dev" :remote "tecosaur")
+   :files
+   (:defaults "etc")
+   :build t :pre-build
+   (with-temp-file "org-version.el"
+     (require 'lisp-mnt)
+     (let
+         ((version
+           (with-temp-buffer
+             (insert-file-contents "lisp/org.el")
+             (lm-header "version")))
+          (git-version
+           (string-trim
+            (with-temp-buffer
+              (call-process "git" nil t nil "rev-parse" "--short" "HEAD")
+              (buffer-string)))))
+       (insert
+        (format "(defun org-release () \"The release version of Org.\" %S)\n" version)
+        (format "(defun org-git-version () \"The truncate git commit hash of Org mode.\" %S)\n" git-version)
+        "(provide 'org-version)\n"))))
+  :pin nil)
+(unpin! org)
 (package! affe)
 (unpin! org-roam)
 (package! org-roam-ui)
 (package! lsp-dart)
 (package! org-modern)
-(package! org-fragtog)
-(package! markdown-mode)
 (package! anki-editor
   :recipe (:host github :repo "anki-editor/anki-editor"))
 (package! copilot
