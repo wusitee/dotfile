4c282eea655516968fdec8e61da819b280325e4d
Siwei Wang <kekdkkwk@outlook.com>
2026-04-06 11:36:17 +0800

    feat(fish): update ff function with custom fastfetch config
    - Change ff alias to function using examples/6.jsonc config
    - Fix indentation consistency in y function

--- fish/config.fish
+++ fish/config.fish
@@ -10,14 +10,17 @@ set PATH $PATH /home/wste/.local/bin
 # init pyenv
 # pyenv init - | source
 # status --is-interactive; and pyenv virtualenv-init - | source
-alias ff=fastfetch
+function ff
+    fastfetch -c examples/6.jsonc
+end
+
 function y
-	set tmp (mktemp -t "yazi-cwd.XXXXXX")
-	yazi $argv --cwd-file="$tmp"
-	if set cwd (command cat -- "$tmp"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
-		builtin cd -- "$cwd"
-	end
-	rm -f -- "$tmp"
+    set tmp (mktemp -t "yazi-cwd.XXXXXX")
+    yazi $argv --cwd-file="$tmp"
+    if set cwd (command cat -- "$tmp"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+        builtin cd -- "$cwd"
+    end
+    rm -f -- "$tmp"
 end
 
 starship init fish | source
